f2fde6d54b066f23a44172c7db8e4497a6ab7e5b
sanjeev yadav <sanjeev.yadav@grafioffshorenepal.com>
2023-06-09 14:56:55 +0545

    update

---
+++ medium-term-accommodation.php
@@ -0,0 +1,128 @@
+<?php include 'header.php'; ?>
+<!-- ================================== -->
+<section class="section">
+  <div class="container-fluid">
+    <div class="inner-banner-wrapp indigo rounded-4">
+      <div class="container">
+        <div class="inner-banner-content">
+          <div class="inner-banner-content-left dark">
+            <h2>Medium-Term Accommodation (MTA)</h2>
+            <p>Experience the Comfort of Medium-Term Accommodation</p>
+            <button class="btn btn-primary d-flex align-items-center me-3" role="button">
+              <span>Call at</span> <span class="material-icons me-2 ms-2">call</span>
+              <span>1300 456 245</span>
+            </button>
+          </div>
+          <div class="inner-banner-content-right">
+            <img src="assets/img/services/istockphoto-605759166-612x6121.png" class="img-fluid" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- ================================== -->
+<section class="section-row-medium">
+  <div class="container">
+    <div class="content-head">
+      <h4 class="mb-4">A type of support offered through the NDIS that helps participants find suitable, safe, and affordable housing for medium-term living purposes.</h4>
+      <p>MTA covers rent and other costs associated with the rental agreement, such as bills, furniture, and household items. MTA offers a dream come true for many with disabilities: the opportunity to live independently but in a supported environment.</p>
+    </div>
+    <div class="row">
+      <div class="col-sm-12 col-md-9">
+        <p>In this medium-term accommodation, you can stay for up to 90 days. However, you might be able to get funding for longer than 90 days if you can show that you need it because of your support needs. Day-to-day living expenses such as food, internet, and power are not covered by funding for medium-term accommodation.</p>
+        <p>
+           It also excludes personal care assistance such as showering, bathing, oral hygiene, dressing, and grooming; toileting, bladder and bowel management, 
+          and menstrual care; eating and drinking; attending appointments; using aids and appliances, hearing and communication devices; mobility and transferring, 
+          such as getting in and out of bed and using the toilet.</p>
+          <p>Our MTA services at Empower Living cover vital areas such as:</p>
+          <div class="listed-wrapp dark mb-4">
+          <ul>
+            <li>
+              <span class="material-icons"> check </span> move-in assistance
+            </li>
+            <li>
+              <span class="material-icons"> check </span> provision of furniture, equipment, and modifications to your home; and home maintenance and repairs
+            </li>
+            <li>
+              <span class="material-icons"> check </span> help with planning meals and food preparation
+            </li>
+            <li>
+              <span class="material-icons"> check </span> meal delivery service
+            </li>
+            <li>
+              <span class="material-icons"> check </span> house cleaning services
+            </li>
+            <li>
+              <span class="material-icons"> check </span> and much more!
+            </li>
+          </ul>
+        </div>
+        <p>If you wish to explore MTA as part of your NDIS funding, do ensure to reach out to us. We will take our time to explain the options available carefully at each stage, 
+          depending on individual eligibility, needs, and budget.</p>
+      </div>
+      <div class="col-sm-12 col-md-3">
+        <div class="text-center">
+          <img src="assets/img/image12.png" class="img-fluid" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- ================================== -->
+<section class="text-white">
+  <div class="container-fluid">
+    <div class="section-row-medium rounded-4 secondary">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-md-4">
+            <h3 class="heading mb-3 text-start">
+              Eligibility
+            </h3>
+            <p>You must meet the following requirements to qualify for this support under the NDIS program:</p>
+          </div>
+          <div class="col-sm-12 col-md-8">
+            <div class="listed-wrapp">
+              <ul>
+                <li>
+                  <span class="material-icons"> check </span> You must be an Australian citizen, a resident of Australia, and a permanent visa holder.
+                </li>
+                <li>
+                  <span class="material-icons"> check </span> After a short period of temporary housing, you will transfer to your permanent residence.
+                </li>
+                <li>
+                  <span class="material-icons"> check </span> You need the accommodation because your disability supports aren't ready yet, so you can't move into your long-term home.
+                </li>
+                <li>
+                  <span class="material-icons"> check </span> For some reason, you cannot remain in your current residence while you await your long-term home.
+                </li>
+                <li>
+                  <span class="material-icons"> check </span> It meets reasonable and necessary criteria.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- ================================== -->
+<section class="section">
+  <div class="container section-row">
+    <div class="row">
+      <div class="col-sm-12 col-md-4">
+        <h3>Why We Should Be Your Medium-Term Accommodation (MTA)</h3>
+      </div>
+      <div class="col-sm-12 col-md-8">
+        <p class="fs-5">Disability can often seem insurmountable, but Empower Living’s Medium Term Accommodation (MTA) support makes traversing it easier. </p>
+        <p>From wheelchair access ramps to personalized transport and round-the-clock staff care, we have it all at our fingertips. Plus, we possess years of shared service experience tracking solutions for your disability needs, meaning the answers that make a difference are the same ones you receive. </p>
+        <p>Empower Living gives you comfort and peace of mind by knowing that professionals are ready and willing to help you through one of life's most challenging journeys.</p>
+        <p class="mb-0">
+        Disability needn’t be a roadblock – give Empower Living a spin and find out where your journey can take you.</p>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- ================================== -->
+<?php include 'footer.php'; ?>
