1916b8fad24bfc5307ef151f160d378e7fe9cc77
sanjeev yadav <sanjeev.yadav@grafioffshorenepal.com>
2023-06-09 13:29:21 +0545

    update

---
+++ case-management.php
@@ -0,0 +1,95 @@
+<?php include 'header.php'; ?>
+<!-- ================================== -->
+<section class="section">
+  <div class="container-fluid">
+    <div class="inner-banner-wrapp indigo rounded-4">
+      <div class="container">
+        <div class="inner-banner-content">
+          <div class="inner-banner-content-left dark">
+            <h2>Case Management</h2>
+            <p>Compassionate Care for Your Needs</p>
+            <button class="btn btn-primary d-flex align-items-center me-3" role="button">
+              <span>Call at</span> <span class="material-icons me-2 ms-2">call</span>
+              <span>1300 456 245</span>
+            </button>
+          </div>
+          <div class="inner-banner-content-right">
+            <img src="assets/img/services/pexels-photo-40582231.png" class="img-fluid" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- ================================== -->
+<section class="section-row-medium text-center">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12 col-md-9">
+        <h4 class="mb-4">An integral part of NDIS support for people with a disability.</h4>
+        <p class="mb-0">It involves creating an individualized plan, providing support for implementing, monitoring, and reviewing that plan, 
+          and providing ongoing knowledge and direction beyond the initial stages.</p>
+      </div>
+      <div class="col-sm-12 col-md-3">
+        <div class="text-center">
+          <img src="assets/img/Service-icon-05.png" class="img-fluid" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- ================================== -->
+<section class="text-white">
+  <div class="container-fluid">
+    <div class="section-row-medium rounded-4 secondary">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-md-4">
+            <h3 class="heading mb-3 text-start">
+              Eligibility
+            </h3>
+            <p>You must fulfill the below criteria as stipulated by NDIS to be a beneficiary of this support.</p>
+          </div>
+          <div class="col-sm-12 col-md-8">
+            <div class="listed-wrapp">
+              <ul>
+                <li>
+                  <span class="material-icons"> check </span> You must be an Australian citizen, a resident of Australia, and a permanent visa holder.
+                </li>
+                <li>
+                  <span class="material-icons"> check </span> You must need disability-specific support to complete daily life activities.
+                </li>
+                <li>
+                  <span class="material-icons"> check </span> You must be between 7 to 65 years of age.
+                </li>
+                <li>
+                  <span class="material-icons"> check </span> The intention helps you pursue your goals and maintain functional capacity.
+                </li>
+                <li>
+                  <span class="material-icons"> check </span> It meets reasonable and necessary criteria.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- ================================== -->
+<section class="section">
+  <div class="container section-row">
+    <div class="row">
+      <div class="col-sm-12 col-md-4">
+        <h3>Why We Should Be Your Case Management Provider</h3>
+      </div>
+      <div class="col-sm-12 col-md-8">
+        <p class="fs-5">At Empower Living, our case managers ensure that every NDIS participant has access to tailored support and resources, giving them the confidence and security to live their life the way they choose.</p>
+        <p>Our case managers provide compassionate guidance, links to trusted help, and direct inclusion in social activities. Plus, our team is experts in navigating the complexities of NDIS plans via regular reviews, ensuring your goals stay on track and you get the most out of your plan. </p>
+        <p class="mb-0">If you're looking for reliable case management support that uniquely understands how disability impacts life - look no further than Empower Living!</p>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- ================================== -->
+<?php include 'footer.php'; ?>
