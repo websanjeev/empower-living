ccd67e7357db48ed46b08429db53f35e56788d56
sanjeev yadav <sanjeev.yadav@grafioffshorenepal.com>
2023-06-09 15:48:40 +0545

    update

---
+++ support-coordination.php
@@ -0,0 +1,122 @@
+<?php include 'header.php'; ?>
+<!-- ================================== -->
+<section class="section">
+  <div class="container-fluid">
+    <div class="inner-banner-wrapp indigo rounded-4">
+      <div class="container">
+        <div class="inner-banner-content">
+          <div class="inner-banner-content-left dark">
+            <h2>Support Coordination</h2>
+            <p>Expert Support for Your Coordination Needs</p>
+            <button class="btn btn-primary d-flex align-items-center me-3" role="button">
+              <span>Call at</span> <span class="material-icons me-2 ms-2">call</span>
+              <span>1300 456 245</span>
+            </button>
+          </div>
+          <div class="inner-banner-content-right">
+            <img src="assets/img/services/photo-1505876104692-2f34b9d543031.png" class="img-fluid" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- ================================== -->
+<section class="section-row-medium">
+  <div class="container">
+    <div class="content-head">
+      <h4 class="mb-4">Disability isn't always easy, but thankfully, with the help of the National Disability Insurance Scheme (NDIS), you don't have to face it alone.</h4>
+      <p>Support coordination provided by providers such as Empower Living is invaluaxble; it assists in making the most of all NDIS services and funding.</p>
+    </div>
+    <div class="row">
+      <div class="col-sm-12 col-md-9">
+        <p>Support coordination involves helping people with a disability create an individualized plan, sourcing support workers and service providers, understanding how programs work, assisting with monitoring goals, and connecting them to services within their local community.</p>
+        <p class="mb-0">
+          Participants of the National Disability Insurance Scheme (NDIS) are offered three levels of Support Coordination to help them get the most out of their NDIS plan.</p>
+      </div>
+      <div class="col-sm-12 col-md-3">
+        <div class="text-center">
+          <img src="assets/img/Frame7705.png" class="img-fluid" alt="">
+        </div>
+      </div>
+    </div>
+    <div class="card-box-wrapper">
+      <div class="row">
+        <div class="col-sm-12 col-md-4">
+          <div class="card-box">
+            <span>Level 1</span>
+            <h4 class="mb-3">Support Connection</h4>
+            <p>Offers assistance to comprehend and make the best use of your plans, connect with appropriate NDIS providers, community, mainstream and other supports; it also increases your confidence and abilities in managing the plan on their own.</p>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-4">
+          <div class="card-box">
+            <span>Level 2</span>
+            <h4 class="mb-3">Coordination of Supports</h4>
+            <p>Helps put in place a range of services with an aim to increase your ability to build relationships, complete tasks, live independently and be part of your community. This level also works towards developing your skills and assurance in taking control of your life.</p>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-4">
+          <div class="card-box">
+            <span>Level 3</span>
+            <h4 class="mb-3">Specialist Support Coordination</h4>
+            <p>The level three offers a higher level support for cases which may require more specialized attention. A specialist coordinator will assist you in managing disturbances within the service environment as well as making sure that facilities are provided consistently.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <p class="text-center mb-0">As a participant, you can either take up support coordination according to what is mentioned in your plan or select the type of support coordination that should be used depending on your needs.</p>
+  </div>
+</section>
+<!-- ================================== -->
+<section class="text-white">
+  <div class="container-fluid">
+    <div class="section-row-medium rounded-4 secondary">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-md-4">
+            <h3 class="heading mb-3 text-start">
+              Eligibility
+            </h3>
+            <p>To get this support through the NDIS program, you must meet the following requirements:</p>
+          </div>
+          <div class="col-sm-12 col-md-8">
+            <div class="listed-wrapp">
+              <ul>
+                <li>
+                  <span class="material-icons"> check </span> You must be an Australian citizen, a resident of Australia, and a permanent visa holder.
+                </li>
+                <li>
+                  <span class="material-icons"> check </span> You must be between 7 to 65 years of age.
+                </li>
+                <li>
+                  <span class="material-icons"> check </span> The intention helps you pursue your goals and develop the skills needed to become better and independent.
+                </li>
+                <li>
+                  <span class="material-icons"> check </span> You don't have an informal support such as family and friends that can connect you to a service provider
+                </li>
+                <li>
+                  <span class="material-icons"> check </span> You are a new NDIS participant receiving the support for the first time
+                </li>
+                <li>
+                  <span class="material-icons"> check </span> It meets reasonable and necessary criteria.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- ================================== -->
+<section class="section">
+  <div class="container section-row text-center">
+    <p>With all this assistance, you can count on Empower Living to safely and effectively ensure that your disability plan objectives are met.
+      Our positions are unique as we provide the right kind of consistent mentoring, advice, and the ability to give you practical support and an extra sense of security as your
+      support coordinator. </p>
+    <p>So if you're looking for ways to empower yourself in managing disability-related issues - start with having Empower Living in your corner!</p>
+  </div>
+</section>
+<!-- ================================== -->
+<?php include 'footer.php'; ?>
