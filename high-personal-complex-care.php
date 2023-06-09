afdf45b3536bf38535862245a896271f5839fd1a
sanjeev yadav <sanjeev.yadav@grafioffshorenepal.com>
2023-06-09 13:09:49 +0545

    update

---
+++ high-personal-complex-care.php
@@ -0,0 +1,101 @@
+<?php include 'header.php'; ?>
+<!-- ================================== -->
+<section class="section">
+  <div class="container-fluid">
+    <div class="inner-banner-wrapp indigo rounded-4">
+      <div class="container">
+        <div class="inner-banner-content">
+          <div class="inner-banner-content-left dark">
+            <h2>High Personal & Complex Care</h2>
+            <p>Comprehensive Care for Optimal Outcomes</p>
+            <button class="btn btn-primary d-flex align-items-center me-3" role="button">
+              <span>Call at</span> <span class="material-icons me-2 ms-2">call</span>
+              <span>1300 456 245</span>
+            </button>
+          </div>
+          <div class="inner-banner-content-right">
+            <img src="assets/img/services/istockphoto-639927062-612x6121.png" class="img-fluid" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- ================================== -->
+<section class="section-row-medium">
+  <div class="container">
+    <div class="content-head">
+      <h4 class="mb-4">It typically involve detailed and ongoing assistance with daily activities such as showering, dressing, and eating - the necessities of life.</h4>
+    </div>
+    <div class="row">
+      <div class="col-sm-12 col-md-9">
+        <p>Complex care is more intensive and can involve medical treatments, specialized therapies, or support for using supports safely in your home. Often, this kind of care is required to maintain the well-being of a person with profound needs due to a severe injury or illness resulting in a disability. </p>
+        <p>A person who accesses an NDIS-approved provider for high personal care or complex care support will receive the necessary time and attention to guarantee their safety, health, well-being, comfort, dignity, and independence.</p>
+        <p class="mb-0">
+          Our duty as your would-be NDIS provider is to ensure that these values remain intact and that you get the utmost care at all times, regardless of the situation.</p>
+      </div>
+      <div class="col-sm-12 col-md-3">
+        <div class="text-center">
+          <img src="assets/img/Service-icon-05.png" class="img-fluid" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- ================================== -->
+<section class="text-white">
+  <div class="container-fluid">
+    <div class="section-row-medium rounded-4 secondary">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-md-4">
+            <h3 class="heading mb-3 text-start">
+              Eligibility
+            </h3>
+            <p>To receive this support under the NDIS plan, you must fulfill the criteria below.</p>
+          </div>
+          <div class="col-sm-12 col-md-8">
+            <div class="listed-wrapp">
+              <ul>
+                <li>
+                  <span class="material-icons"> check </span> You must be an Australian citizen, a resident of Australia, and a permanent visa holder.
+                </li>
+                <li>
+                  <span class="material-icons"> check </span> You must need disability-specific support to complete daily life activities.
+                </li>
+                <li>
+                  <span class="material-icons"> check </span> You must be between 7 to 65 years of age.
+                </li>
+                <li>
+                  <span class="material-icons"> check </span> The intention helps you pursue your goals and maintain functional capacity.
+                </li>
+                <li>
+                  <span class="material-icons"> check </span> It meets reasonable and necessary criteria.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- ================================== -->
+<section class="section">
+  <div class="container section-row">
+    <div class="row">
+      <div class="col-sm-12 col-md-4">
+        <h3>Why We Should Be Your High Personal and Complex Care Provider</h3>
+      </div>
+      <div class="col-sm-12 col-md-8">
+        <p class="fs-5">At Empower Living, we understand that many factors come into play when providing high and complex care for those with particular needs. To this end, our team of highly knowledgeable and experienced staff offers a wide range of services to cover all bases: </p>
+        <p>From personal hygiene needs (e.g., showering, bathing, oral hygiene, dressing and grooming) to mobility (moving in or out of bed, around the house etc.), help during eating/drinking, bladder/bowel management and menstrual care, to attending appointments for personal and complex care. </p>
+        <p>Also, for high-care needs such as cerebral palsy, acquired brain injury, spinal cord injuries, multiple sclerosis, muscular dystrophy, gastrostomy feed needs (PEG/catheter) and complex bowel management, among others, our team at Empower Living is more than capable, of providing expert support, including medication supervision and administration. </p>
+        <p class="mb-0">
+          We understand that each individual has personality-based preferences; therefore, the choice is always considered when selecting a carer. We accommodate different personality traits so you can feel entirely supported throughout your journey with us.</p>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- ================================== -->
+<?php include 'footer.php'; ?>
---
+++ life-skills-training.php
@@ -0,0 +1,143 @@
+<?php include 'header.php'; ?>
+<!-- ================================== -->
+<section class="section">
+  <div class="container-fluid">
+    <div class="inner-banner-wrapp indigo rounded-4">
+      <div class="container">
+        <div class="inner-banner-content">
+          <div class="inner-banner-content-left dark">
+            <h2>Life Skills Training</h2>
+            <p>Transformative Life Skills Training with a Difference</p>
+            <button class="btn btn-primary d-flex align-items-center me-3" role="button">
+              <span>Call at</span> <span class="material-icons me-2 ms-2">call</span>
+              <span>1300 456 245</span>
+            </button>
+          </div>
+          <div class="inner-banner-content-right">
+            <img src="assets/img/services/istockphoto-1270715052-612x6121.png" class="img-fluid" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- ================================== -->
+<section class="section-row-medium">
+  <div class="container">
+    <div class="content-head">
+      <h4 class="mb-4">An important part of NDIS support, helping people with disabilities become independent and better manage their daily lives.</h4>
+      <p>This training can include skills like budgeting, managing a household, getting around town, developing personal relationships, and more. Through life skills training, NDIS participants are able to learn practical skills that will help them become more involved in their community and lead more fulfilling lives.</p>
+    </div>
+    <div class="row">
+      <div class="col-sm-12 col-md-9">
+        <p>At Empower Living, we offer all-inclusive training and support to help individuals gain the most important skills for leading a financially secure, independent livelihood. </p>
+        <p>Our life skills training programs will be structured according to your specific needs; from helping you develop essential interpersonal communication skills and improving your hygiene to developing mindsets that promote healthy relationships. Our training will also create lasting, beneficial changes in how you think, interact, and operate in your daily life. </p>
+        <p>Now, all these will be delivered by NDIS-certified professionals who understand the challenges people face when learning and practicing new life skills.</p>
+        <p class="mb-0">Subscribing to Empower Living’s life skill training allows you to unlock your potential, equip yourself with the necessary tools for success, and develop into the kind of person who can take on whatever challenge comes your way confidently.</p>
+      </div>
+      <div class="col-sm-12 col-md-3">
+        <div class="text-center">
+          <img src="assets/img/Service-icon-04.png" class="img-fluid" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- ================================== -->
+<section class="text-white">
+  <div class="container-fluid">
+    <div class="section-row-medium rounded-4 secondary">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-md-4">
+            <h3 class="heading mb-3 text-start">
+              Eligibility
+            </h3>
+          </div>
+          <div class="col-sm-12 col-md-8">
+            <div class="listed-wrapp">
+              <ul>
+                <li>
+                  <span class="material-icons"> check </span> You must be an Australian citizen, a resident of Australia, and a permanent visa holder.
+                </li>
+                <li>
+                  <span class="material-icons"> check </span> You must be disabled as a result of an impairment.
+                </li>
+                <li>
+                  <span class="material-icons"> check </span> You must be between the ages of 7 and 65.
+                </li>
+                <li>
+                  <span class="material-icons"> check </span> Your disability limits your capacity to work, study, or participate in social activities.
+                </li>
+                <li>
+                  <span class="material-icons"> check </span> You’ll likely require NDIS supports your entire life
+                </li>
+                <li>
+                  <span class="material-icons"> check </span> The intention helps you pursue your goals and maintain functional capacity
+                </li>
+                <li>
+                  <span class="material-icons"> check </span> It meets reasonable and necessary criteria.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- ================================== -->
+<section class="section">
+  <div class="container section-row">
+    <div class="row">
+      <div class="col-sm-12 col-md-4">
+        <h3>Why It's Imperative you participate in a Life Skill Training</h3>
+      </div>
+      <div class="col-sm-12 col-md-8">
+        <div class="row">
+          <div class="col-sm-12 col-md-6">
+            <div class="card-block-wrapp">
+              <h4>A sense of accomplishment</h4>
+              <p>Learning new skills can be challenging, but it can also be very rewarding.
+                Our life skills training will provide you with a sense of pride and satisfaction that comes
+                from knowing you are capable of doing things that others may have thought were impossible.</p>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-6">
+            <div class="card-block-wrapp">
+              <h4>Improved self-esteem</h4>
+              <p>Learning new skills and becoming more independent can help you challenge discrimination, 
+                negative perceptions, and give you a greater sense of self-worth.</p>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-6">
+            <div class="card-block-wrapp">
+              <h4>Greater independence</h4>
+              <p>For many people with disabilities, being able to do things for themselves can be empowering.
+                Our life skills training will help you live more independently by teaching you how to do things
+                like cook, clean, and manage your finances.</p>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-6">
+            <div class="card-block-wrapp">
+              <h4>Enhanced employability</h4>
+              <p>Life skills training can enhance your employability by teaching you new skills that can make
+                you more attractive to potential employers. Also, our training in life skills will help you
+                become more independent and self-sufficient, which are traits that many employers like to see.</p>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-6">
+            <div class="card-block-wrapp">
+              <h4>Improved quality of life</h4>
+              <p>Learning new skills will help you actively participate in activities you enjoy,
+                make friends, and feel more connected to your community. All of these things can contribute
+                to a greater sense of well-being and a higher quality of life.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- ================================== -->
+<?php include 'footer.php'; ?>
