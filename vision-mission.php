79f739196860b9978298c6486d8278f330af4ca6
sanjeev yadav <sanjeev.yadav@grafioffshorenepal.com>
2023-06-11 15:36:19 +0545

    update

---
+++ vision-mission.php
@@ -0,0 +1,103 @@
+<?php include 'header.php'; ?>
+<!-- ================================== -->
+<section class="section">
+  <div class="container-fluid">
+    <div class="section-row-medium green rounded-4">
+      <div class="container">
+        <div class="about-inner-banner">
+          <h2>A team of <span>energized, committed</span> & <span>dedicated</span> individuals working together to enhance lives.</h2>
+        </div>
+        <div class="about-empower-block-wrapp">
+          <div class="empower-block">
+            <div class="empower-icon-wrapp">
+              <span class="material-icons">people_outline</span>
+            </div>
+            <h3>100+</h3>
+            <p>Certified Staffs</p>
+          </div>
+
+          <div class="empower-block">
+            <div class="empower-icon-wrapp">
+              <span class="material-icons">verified</span>
+            </div>
+            <h3>200+</h3>
+            <p>Satisfied Clients</p>
+          </div>
+
+          <div class="empower-block">
+            <div class="empower-icon-wrapp">
+              <span class="material-icons">verified_user</span>
+            </div>
+            <h3>2017</h3>
+            <p>Year Founded</p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+</section>
+<!-- ================================== -->
+<section class="section-row-large pb-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12 col-md-4">
+        <h3>About Empower Living</h3>
+      </div>
+      <div class="col-sm-12 col-md-8">
+        <p class="fs-5">Empower Living is an inspiring organization in the Australian community, bringing together a team of young professionals dedicated to realizing, respecting, and upholding the human rights, independence and freedom of all people with disabilities, while supporting the individuals and their families to enhance their quality of life.</p>
+        <p>We aspire to change lives positively and are dedicated to being one of the best providers in the area.</p>
+        <p>Our reputation as a NDIS support provider is built on valuing the dreams of our participants (clients), empowering them to achieve what was perceived to be impossible, and accompanying them and their families in changing their lives for the better.</p>
+        <h4>We take pride in our holistic approach to care, assist, support and connect individuals and professionals with a personalised plan that is developed and tailored to their needs.</h4>
+        <p class="fs-5">Our skilled team will not only inspire and empower you but will also help you see things from a new perspective, encouraging you to dream big and not let your disability define you.</p>
+        <p>From participants to staff members, Empower Living as a whole is passionate about guiding, supporting, and empowering individuals and creating opportunities for personal and professional skills development.</p>
+        <p>Our biggest goal is to impact lives by building bridges and connections, and we are genuinely committed to helping our clients achieve the impossible.</p>
+        <p>Join us at Empower Living, where we acknowledge every victory, celebrate diversity, inspire independence and empower people with disability to live their life to their fullest potential!.</p>
+        <p class="fs-5 mb-0">Together, let’s make the impossible possible!</p>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- ================================== -->
+<section class="section-row">
+  <div class="container">
+    <h2 class="heading mb-5">What our clients say</h2>
+    <div class="testimonials-items">
+      <div class="testimonials-block indigo">
+        <img src="assets/img/quote.svg" class="img-fluid" alt="">
+        <p>The NDIS has done wonders for people with disability and their family.
+          Invest in and empower people with disability by supporting them to achieve their goals and
+          participate in the community and economy.</p>
+        <span class="clients-name">Brandon Aokiji</span>
+        <span class="clients-add">Canberra, Aus</span>
+      </div>
+      <div class="testimonials-block gray-200">
+        <img src="assets/img/quote.svg" class="img-fluid" alt="">
+        <p>The NDIS has done wonders for people with disability and their family.
+          Invest in and empower people with disability by supporting them to achieve their goals and
+          participate in the community and economy.</p>
+        <span class="clients-name">Brandon Aokiji</span>
+        <span class="clients-add">Canberra, Aus</span>
+      </div>
+      <div class="testimonials-block gray-300">
+        <img src="assets/img/quote.svg" class="img-fluid" alt="">
+        <p>The NDIS has done wonders for people with disability and their family.
+          Invest in and empower people with disability by supporting them to achieve their goals and
+          participate in the community and economy.</p>
+        <span class="clients-name">Brandon Aokiji</span>
+        <span class="clients-add">Canberra, Aus</span>
+      </div>
+      <div class="testimonials-block indigo">
+        <img src="assets/img/quote.svg" class="img-fluid" alt="">
+        <p>The NDIS has done wonders for people with disability and their family.
+          Invest in and empower people with disability by supporting them to achieve their goals and
+          participate</p>
+        <span class="clients-name">Brandon Aokiji</span>
+        <span class="clients-add">Canberra, Aus</span>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- ================================== -->
+<?php include 'footer.php'; ?>
