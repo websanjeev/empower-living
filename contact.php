f04b7c0d4ea67772095f529d5d6b6e4ffbb254bc
sanjeev yadav <sanjeev.yadav@grafioffshorenepal.com>
2023-06-15 08:29:33 +0545

    update

---
+++ contact.php
@@ -0,0 +1,110 @@
+<?php include 'header.php'; ?>
+<!-- ================================== -->
+<section class="section-row pt-5">
+  <div class="container">
+    <div class="about-head">
+      <h2>Hey There! <span class="gray">How Can We Help?</span></h2>
+    </div>
+    <div class="contact-main-wrapper">
+      <div class="row">
+        <div class="col-sm-12 col-md-4">
+          <div class="contact-main-block text-center gray-200">
+            <div class="large-icon-wrapp">
+              <img src="assets/img/messanger.png" class="img-fluid" alt="">
+            </div>
+            <h4>I'd like assistance with the messenger</h4>
+            <a class="btn btn-primary btn-block" href="#" role="button">Chat with us now
+              <span class="material-icons ms-1">chat_bubble_outline</span>
+            </a>
+            <p>Or email us at empowerliving@mail.com</p>
+            <span class="contact-nu">
+              <span class="material-icons">call</span> 1300 244 664
+            </span>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-8">
+          <div class="contact-main-block indigo">
+            <h4 class="text-left">I'd like to get in touch</h4>
+            <form>
+              <div class="mb-3">
+                <input class="form-control form-control-lg" type="text" placeholder="Full name" aria-label="">
+              </div>
+              <div class="mb-3">
+                <input class="form-control form-control-lg" type="number" placeholder="Phone" aria-label="">
+              </div>
+              <div class="mb-3">
+                <input class="form-control form-control-lg" type="email" placeholder="Email" aria-label="">
+              </div>
+              <div class="mb-3">
+                <textarea class="form-control" placeholder="Your query" rows="3"></textarea>
+              </div>
+              <button class="btn btn-primary mb-0" role="button">Send your query</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="contact-block-wrapper">
+      <div class="row">
+        <div class="col-sm-12 col-md-4">
+          <div class="contact-block-inner">
+            <h4>Branch 1</h4>
+            <ul>
+              <li>
+                <span class="material-icons">place</span>
+                <p>25 Dickson Place, Canberra, ACT, Australia, Australian Capital Territory</p>
+              </li>
+              <li>
+                <span class="material-icons">call</span>
+                <p>1300 244 664</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-4">
+          <div class="contact-block-inner">
+            <h4>Branch 2</h4>
+            <ul>
+              <li>
+                <span class="material-icons">place</span>
+                <p>25 Dickson Place, Canberra, ACT, Australia, Australian Capital Territory</p>
+              </li>
+              <li>
+                <span class="material-icons">call</span>
+                <p>1300 244 664</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-4">
+          <div class="contact-block-inner">
+            <h4>Branch 3</h4>
+            <ul>
+              <li>
+                <span class="material-icons">place</span>
+                <p>25 Dickson Place, Canberra, ACT, Australia, Australian Capital Territory</p>
+              </li>
+              <li>
+                <span class="material-icons">call</span>
+                <p>1300 244 664</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="contact-social-links-wrapper">
+      <h4>Connect with us on social</h4>
+      <div class="social-link-wrapp">
+        <ul>
+          <li><a href="#"><img src="assets/img/social-links/facebook.png" class="img-fluid" alt=""></a></li>
+          <li><a href="#"><img src="assets/img/social-links/twiteer.png" class="img-fluid" alt=""></a></li>
+          <li><a href="#"><img src="assets/img/social-links/instagram.png" class="img-fluid" alt=""></a></li>
+          <li><a href="#"><img src="assets/img/social-links/linkeding.png" class="img-fluid" alt=""></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- ================================== -->
+<?php include 'footer.php'; ?>
