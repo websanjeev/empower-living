4a807d172b885237de0d489d35870927879ed9b9
sanjeev yadav <sanjeev.yadav@grafioffshorenepal.com>
2023-06-18 17:51:17 +0545

    update design

--- header.php
+++ header.php
@@ -1,39 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Empower Living</title>
-    <!-- ========== Material icons  -->
-    <link rel="stylesheet"
-      href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
-    <!-- ========= Slick carousel -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css">
-    <!-- ====================== -->
-    <link rel="stylesheet" href="assets/css/style.css" />
-  </head>
-  <body>
-    <header>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Empower Living</title>
+  <!-- ========== Material icons  -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <!-- ========= Slick carousel -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css">
+  <!-- ====================== -->
+  <link rel="stylesheet" href="assets/css/style.css" />
+</head>
+
+<body>
+  <header>
     <section class="top-navbar-wrapp">
       <div class="container">
         <div class="d-flex justify-content-end">
-          <button
-            type="button"
-            class="btn btn-outline-secondary btn-sm d-flex align-items-center btn-icon me-2"
-          >
+          <button type="button" class="btn btn-outline-secondary btn-sm d-flex align-items-center btn-icon me-2">
             <span class="material-icons">accessibility_new</span>
             Accessibility
           </button>
           <div class="search-wrapp">
             <div class="form-group">
               <span class="material-icons form-control-feedback">search</span>
-              <input
-                type="text"
-                class="form-control form-control-sm"
-                placeholder="Search"
-              />
+              <input type="text" class="form-control form-control-sm" placeholder="Search" />
             </div>
           </div>
         </div>
@@ -41,143 +35,84 @@
     </section>
     <!-- ====================================== -->
     <section class="main-navbar-wrapp">
-      <nav class="navbar navbar-expand-lg">
-        <div class="container">
-          <a class="navbar-brand empower-living" href="index.php"
-            ><img src="assets/img/empower-living.svg" class="img-fluid" alt=""
-          /></a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto ms-auto mb-2 mb-lg-0">
-              <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Services
-                </a>
-                <ul class="dropdown-menu">
-                  <div class="row">
-                    <div class="col-sm-12 col-md-4">
-                      <ul>
-                        <li><a class="dropdown-item" href="supported-independent-living.php">Supported independent living (SIL)</a></li>
-                        <li>
-                          <a class="dropdown-item" href="community-access.php">Community Access</a>
-                        </li>
-                        <li>
-                          <a class="dropdown-item" href="higher-intensity-mental-health-support.php"
-                            >Higher-Intensity Mental Health Support</a
-                          >
-                        </li>
-                      </ul>
-                    </div>
-                    <div class="col-sm-12 col-md-4">
-                      <ul>
-                        <li><a class="dropdown-item" href="individualized-living-options.php">Individualized living options (ILO)</a></li>
-                        <li>
-                          <a class="dropdown-item" href="short-term-accommodation-respite.php">Short Term Accommodation(STA)/Respite</a>
-                        </li>
-                        <li>
-                          <a class="dropdown-item" href="cleaning-services.php"
-                            >Cleaning</a
-                          >
-                        </li>
-                        <li>
-                          <a class="dropdown-item" href="life-skills-training.php"
-                            >Life Skills Training</a
-                          >
-                        </li>
-                      </ul>
-                    </div>
-                    <div class="col-sm-12 col-md-4">
-                      <ul>
-                        <li><a class="dropdown-item" href="high-personal-complex-care.php">High Personal & Complex Care</a></li>
-                        <li>
-                          <a class="dropdown-item" href="case-management.php">Case Management</a>
-                        </li>
-                        <li>
-                          <a class="dropdown-item" href="medium-term-accommodation.php"
-                            >Medium Term Accommodation (MTA)</a
-                          >
-                        </li>
-                        <li>
-                          <a class="dropdown-item" href="support-coordination.php"
-                            >Support Coordination</a
-                          >
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </ul>
-              </li>
-
-              <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  NDIS
-                </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="ndis.php">National Disability Insurance Scheme (NDIS)</a></li>
-                  <li><a class="dropdown-item" href="ndis-stepes.php">NDIS in 6 Easy Steps</a></li>
-                  <li>
-                    <a class="dropdown-item" href="ndis-faq.php">NDIS FAQs</a>
-                  </li>
-                </ul>
-              </li>
-
-              <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  About
-                </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="about-empower-living.php">About Empower Living</a></li>
-                  <li><a class="dropdown-item" href="vision-mission.php">Vision, Mission & Purpose</a></li>
-                  <li>
-                    <a class="dropdown-item" href="leadership-team.php">Leadership Team</a>
-                  </li>
-                </ul>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link" href="blog.php">Blog</a>
-              </li>
-            </ul>
-            <a
-              class="btn btn-primary d-flex align-items-center"
-              href="#"
-              role="button"
-              >Contact Now
-              <span class="material-icons ms-2"> arrow_forward </span>
-            </a>
-          </div>
-        </div>
-      </nav>
+      <div class="container">
+        <nav class="navbar navbar-expand-lg">
+          <div class="container">
+            <a class="navbar-brand" href="index.php"><img src="assets/img/empower-living.svg" class="img-fluid" alt=""></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main_nav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="main_nav">
+              <ul class="navbar-nav me-auto ms-auto">
+                <li class="nav-item active dropdown has-megamenu">
+                  <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown"> Services </a>
+                  <div class="dropdown-menu megamenu" role="menu">
+                  <h4>Services</h4>
+                    <div class="row g-3">
+                      <div class="col-lg-4 col-6">
+                        <div class="col-megamenu services">
+                          <ul class="list-unstyled">
+                            <li><a href="supported-independent-living.php">Supported independent living (SIL)</a></li>
+                            <li><a href="community-access.php">Community Access</a></li>
+                            <li><a href="higher-intensity-mental-health-support.php">Higher-Intensity Mental Health Support</a></li>
+                          </ul>
+                        </div> <!-- col-megamenu.// -->
+                      </div><!-- end col-3 -->
+                      <div class="col-lg-4 col-6">
+                        <div class="col-megamenu">
+                          <ul class="list-unstyled">
+                            <li><a href="individualized-living-options.php">Individualized living options (ILO)</a></li>
+                            <li><a href="short-term-accommodation-respite.php">Short Term Accommodation(STA)/Respite</a></li>
+                            <li><a href="cleaning-services.php">Cleaning</a></li>
+                            <li><a href="life-skills-training.php">Life Skills Training</a></li>
+                          </ul>
+                        </div> <!-- col-megamenu.// -->
+                      </div><!-- end col-3 -->
+                      <div class="col-lg-4 col-6">
+                        <div class="col-megamenu">
+                          <ul class="list-unstyled">
+                            <li><a href="high-personal-complex-care.php">High Personal & Complex Care</a></li>
+                            <li><a href="case-management.php">Case Management</a></li>
+                            <li><a href="medium-term-accommodation.php">Medium Term Accommodation (MTA)</a></li>
+                            <li><a href="support-coordination.php">Support Coordination</a></li>
+                          </ul>
+                        </div> <!-- col-megamenu.// -->
+                      </div>
+                    </div><!-- end row -->
+                  </div> <!-- dropdown-mega-menu.// -->
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link  dropdown-toggle" href="#" data-bs-toggle="dropdown">NDIS</a>
+                  <ul class="dropdown-menu dropdown-menu-end">
+                    <li><a class="dropdown-item" href="ndis.php">National Disability Insurance Scheme (NDIS)</a></li>
+                    <li><a class="dropdown-item" href="ndis-stepes.php">NDIS in 6 Easy Steps</a></li>
+                    <li>
+                      <a class="dropdown-item" href="ndis-faq.php">NDIS FAQs</a>
+                    </li>
+                  </ul>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link  dropdown-toggle" href="#" data-bs-toggle="dropdown">About</a>
+                  <ul class="dropdown-menu dropdown-menu-end">
+                    <li><a class="dropdown-item" href="about-empower-living.php">About Empower Living</a></li>
+                    <li><a class="dropdown-item" href="vision-mission.php">Vision, Mission &amp; Purpose</a></li>
+                    <li>
+                      <a class="dropdown-item" href="leadership-team.php">Leadership Team</a>
+                    </li>
+                  </ul>
+                </li>
+                <li class="nav-item"><a class="nav-link" href="blog.php"> Blog </a></li>
+              </ul>
+              <a class="btn btn-primary d-flex align-items-center" href="contact.php" role="button">Contact Now
+                <span class="material-icons ms-2"> arrow_forward </span>
+              </a>
+            </div> <!-- navbar-collapse.// -->
+          </div> <!-- container-fluid.// -->
+        </nav>
+      </div>
     </section>
     <!-- ====================================== -->
-    </header>
-  </body>
+  </header>
+</body>
+
 </html>
